d03bc0981a4322102e9ad23cbf7256615246ab11
Avinash <avinashg12291992@gmail.com>
2017-10-17 02:19:42 +0530

    using JSQMessagesViewController pod
    build chat UI using JSQMessagesViewController, sending texts

--- GoChat/ChatViewController.swift
+++ GoChat/ChatViewController.swift
@@ -7,15 +7,43 @@
 //
 
 import UIKit
-
-class ChatViewController: UIViewController {
-
+import JSQMessagesViewController
+class ChatViewController: JSQMessagesViewController {
+    var messages = [JSQMessage]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.senderId = "1"
+        self.senderDisplayName = "Avinash"
         // Do any additional setup after loading the view.
     }
-
+    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        print("\(text)")
+        print("didPressSend")
+        messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, text: text))
+        collectionView.reloadData()
+        print(messages)
+    }
+    override func didPressAccessoryButton(_ sender: UIButton!) {
+        print("didPressAccessoryButton")
+    }
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let bubbleFactory = JSQMessagesBubbleImageFactory()
+        return bubbleFactory?.outgoingMessagesBubbleImage(with: UIColor.darkGray)
+    }
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("number of messages:\(messages.count)")
+        return messages.count
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
+        return cell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
